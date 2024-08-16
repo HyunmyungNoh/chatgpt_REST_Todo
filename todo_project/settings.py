ab5429760c2e8325dee34d25f73ab846a182508a
HyunmyungNoh <nhm91@naver.com>
2024-08-16 16:25:12 +0900

    Correct Allowed host

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q&acjpc!!elk#gvoa7b$)e+g=vn6k)n^1+3m7*bq$(rs9-4b4d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://chatgpt-rest-todo.onrender.com', 'localhose', '127.0.0.1']
+ALLOWED_HOSTS = ['https://chatgpt-rest-todo.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
